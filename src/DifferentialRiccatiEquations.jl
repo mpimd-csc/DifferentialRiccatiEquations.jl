42d94d76265c2e2e2fa5326c0fe9f38098992a0a
Jonas Schulze <jschulze@mpi-magdeburg.mpg.de>
2021-05-06 14:55:14 +0200

    Add scaffold

--- src/DifferentialRiccatiEquations.jl
+++ src/DifferentialRiccatiEquations.jl
@@ -1,5 +1,22 @@
 module DifferentialRiccatiEquations
 
-# Write your package code here.
+import CommonSolve: solve
+
+struct GDREProblem
+end
+
+struct DRESolution
+end
+
+struct Ros1 end
+struct Ros2 end
+struct Ros3 end
+struct Ros4 end
+
+include("dense_ros1.jl")
+
+export solve
+export GDREProblem
+export Ros1, Ros2, Ros3, Ros4
 
 end
---
+++ src/dense_ros1.jl
@@ -0,0 +1,6 @@
+function solve(
+    prob::GDREProblem,
+    alg::Ros1;
+)
+    return DRESolution()
+end
