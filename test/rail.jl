2b123829251cd97780c709b0004ad7a18a6c397c
Jonas Schulze <jonas.schulze@st.ovgu.de>
2021-10-12 11:16:36 +0200

    Refactor tests
    Test coverage looked suspiciously low before. Maybe CI didn't realize
    that `test/rail.jl` should generate coverage for more than just one
    algorithm. So, be explicit about this and "degrade" `test/rail.jl` to a
    smoke test.

--- test/rail.jl
+++ test/rail.jl
@@ -13,7 +13,8 @@ tspan = (4500., 0.) # backwards in time
 prob = GDREProblem(Ed, A, B, C, X0, tspan)
 
 # Solve
+dt = @isdefined(dt) ? dt : -1500
 alg = @isdefined(alg) ? alg : Ros1()
-sol = solve(prob, alg; dt=-1500)
+sol = solve(prob, alg; dt=dt)
 
 @test sol isa DREs.DRESolution
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,18 @@
 using DifferentialRiccatiEquations
-using Test
+using MAT, UnPack, Test
+
+const DREs = DifferentialRiccatiEquations
+
+# Setup
+P = matread(joinpath(@__DIR__, "Rail371.mat"))
+@unpack E, A, B, C, X0 = P
+Ed = collect(E) # d=dense
+tspan = (4500., 0.) # backwards in time
+prob = GDREProblem(Ed, A, B, C, X0, tspan)
 
 @testset "DifferentialRiccatiEquations.jl" begin
-    @testset "$alg" for alg in (Ros1(), Ros2())
-        include("rail.jl")
+    @testset "$alg" for alg in (Ros1(), Ros2(), Ros3(), Ros4())
+        sol = solve(prob, alg; dt=-1500)
+        @test sol isa DREs.DRESolution
     end
 end
