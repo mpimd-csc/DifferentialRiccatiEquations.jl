fbbbfac0deb60445ea1b3f7bbe43092d880f0f1e
Jonas Schulze <jschulze@mpi-magdeburg.mpg.de>
2025-04-01 11:32:22 +0200

    Remove superfluous method of _zeros

--- src/util/_zeros.jl
+++ src/util/_zeros.jl
@@ -1,11 +1,10 @@
 # This file is a part of DifferentialRiccatiEquations. License is MIT: https://spdx.org/licenses/MIT.html
 
-function _zeros(T, m::Int, n::Int)
+function _zeros(::Type{T}, m::Int, n::Int=m) where {T}
     X = similar(T, m, n)
     fill!(X, zero(eltype(X)))
 end
 
 _zeros(::Type{T}, _::Int, _::Int) where {T <: UniformScaling} = zero(T)
-_zeros(::Type{Matrix{T}}, m::Int, n::Int=m) where {T} = zeros(T, m, n)
 _zeros(::Type{Symmetric{T, S}}, m::Int, n::Int=m) where {S, T} = Symmetric(_zeros(S, m, n))
 _zeros(::Type{<:SparseArrays.SparseMatrixCSC{T}}, m::Int, n::Int=m) where {T} = spzeros(T, m, n)
